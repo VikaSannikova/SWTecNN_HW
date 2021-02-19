d0b3bfa86a7c50bdf685555c15ed8a9455a359ae
Sannikova Viktoria <vsannikova@VSANNIKOVA-A1.local>
2021-02-19 19:12:16 +0300

    add realisation for two loaders

--- app/src/main/java/com/example/myweather/MainActivity.java
+++ app/src/main/java/com/example/myweather/MainActivity.java
@@ -24,7 +24,7 @@ import com.example.myweather.api.model.WeatherForecast;
 import java.util.ArrayList;
 
 
-public class MainActivity  extends AppCompatActivity implements LoaderManager.LoaderCallbacks<WeatherForecast> {
+public class MainActivity extends AppCompatActivity {
     TextView notification_button;
     CheckBox sprinkler_button;
     RecyclerView weather_list;
@@ -35,11 +35,11 @@ public class MainActivity  extends AppCompatActivity implements LoaderManager.Lo
     ArrayList<DayWeather> days_weather_from_server;
     TextView temp_num, humidity_num;
     LoaderManager loaderManager;
-    public static final int LOADER_CD_ID = 1;
-    public static final int LOADER_W_ID = 2;
+    public static final int LOADER_CD_ID = 2;
+    public static final int LOADER_W_ID = 1;
 
     @Override
-    protected void onCreate(Bundle saveInstanceState){
+    protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.constraint_activity_main);
@@ -51,7 +51,13 @@ public class MainActivity  extends AppCompatActivity implements LoaderManager.Lo
         humidity_num = (TextView) findViewById(R.id.humidity_num);
 
         Bundle bundle = new Bundle();
-        getLoaderManager().initLoader(1, bundle,this).forceLoad();
+        //getLoaderManager().initLoader(1, bundle,this).forceLoad();
+         MydailyLoaderManager day_manager = new MydailyLoaderManager();
+         getLoaderManager().initLoader(LOADER_CD_ID, bundle, day_manager).forceLoad();
+         MyWeakLoaderManager weak_manager = new MyWeakLoaderManager();
+         getLoaderManager().initLoader(LOADER_W_ID, bundle, weak_manager).forceLoad();
+
+
 
 
         days_weather.clear();
@@ -61,9 +67,9 @@ public class MainActivity  extends AppCompatActivity implements LoaderManager.Lo
 
         ArrayList<Location> locations = new ArrayList<>();
         locations.clear();
-        locations.add(new Location( false , "BACKYARD", true));
+        locations.add(new Location(false, "BACKYARD", true));
         locations.add(new Location(false, "BACK PATIO", false));
-        locations.add(new Location( false , "FRONT YARD", false));
+        locations.add(new Location(false, "FRONT YARD", false));
         locations.add(new Location(false, "GARDEN", true));
         locations.add(new Location(false, "PORCH", false));
 
@@ -82,11 +88,11 @@ public class MainActivity  extends AppCompatActivity implements LoaderManager.Lo
         location_list.setAdapter(locationAdapter);
 
         sprinkler_button = (CheckBox) findViewById(R.id.sprinkler_cb);
-        sprinkler_button.setOnClickListener(new View.OnClickListener(){
+        sprinkler_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!sprinkler_button.isChecked()){
-                    for(Location loc : locations){
+                if (!sprinkler_button.isChecked()) {
+                    for (Location loc : locations) {
                         loc.setTomorrowID(false);
                     }
                     MyLocationAdapter locationAdapter1 = new MyLocationAdapter(context, locations);
@@ -95,27 +101,28 @@ public class MainActivity  extends AppCompatActivity implements LoaderManager.Lo
             }
         });
 
-        notification_button = (TextView)findViewById(R.id.notification_button);
+        notification_button = (TextView) findViewById(R.id.notification_button);
         String desc = (String) notification_button.getContentDescription();
-        notification_button.setContentDescription( desc + notification_button.getText());
+        notification_button.setContentDescription(desc + notification_button.getText());
 
-        guideline1 = (Guideline)findViewById(R.id.guideline1);
-        guideline2 = (Guideline)findViewById(R.id.guideline2);
+        guideline1 = (Guideline) findViewById(R.id.guideline1);
+        guideline2 = (Guideline) findViewById(R.id.guideline2);
 
         myview = (WateringTimeView) findViewById(R.id.wateringtimeview);
         myview.setTemplate("already watering for: \r %s");
         Time time = new Time();
         time.setToNow();
-        myview.setDuration(time.hour*3600+time.minute*60+time.second);
+        myview.setDuration(time.hour * 3600 + time.minute * 60 + time.second);
 
     }
+
     @Override
     public void onConfigurationChanged(@NonNull Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
             guideline1.setGuidelinePercent(0.30f);
             guideline2.setGuidelinePercent(0.60f);
-        } else  if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             guideline1.setGuidelinePercent(0.30f);
             guideline2.setGuidelinePercent(0.70f);
         }
@@ -131,98 +138,62 @@ public class MainActivity  extends AppCompatActivity implements LoaderManager.Lo
         super.onResume();
     }
 
-    @Override
-    public Loader<WeatherForecast> onCreateLoader(int id, Bundle args) {
-        Loader<CurrentWeather> mLoader = null;
-        Loader<WeatherForecast> mwl = null;
-        // условие можно убрать, если вы используете только один загрузчик
-        if (id == LOADER_CD_ID) {
-            mLoader = new MyAsyncTaskLoaderCurrDay(this, args);
-            mwl = new MyAsyncTaskLoaderWeak(this, args);
-            Log.d("___", "onCreateLoader CurrDay");
-        }
-        //return mLoader;
-        return mwl;
-    }
+    class MyWeakLoaderManager implements LoaderManager.LoaderCallbacks<WeatherForecast> {
 
-
-
-    @Override
-    public void onLoadFinished(Loader<WeatherForecast> loader, WeatherForecast data) {
-        Log.d("___", "load finished");
-        if (data != null){
-            for (DailyForecast df : data.getDaily()) {
-                days_weather_from_server.add(new DayWeather(df.getDate(), R.drawable.cloudy, (int) df.getTemp().component1()));
+        @Override
+        public Loader<WeatherForecast> onCreateLoader(int id, Bundle args) {
+            Loader<WeatherForecast> mwl = null;
+            if (id == LOADER_W_ID) {
+                mwl = new MyAsyncTaskLoaderWeak(getLayoutInflater().getContext(), args);
+                Log.d("___", "onCreateLoader Weak");
             }
-            MyWeatherAdapter weatherAdapter = new MyWeatherAdapter(this, days_weather_from_server);
-            weather_list.setAdapter(weatherAdapter);
+            return mwl;
         }
 
+        @Override
+        public void onLoadFinished(Loader<WeatherForecast> loader, WeatherForecast data) {
+            Log.d("___", "load finished");
+            if (data != null) {
+                for (DailyForecast df : data.getDaily()) {
+                    days_weather_from_server.add(new DayWeather(df.getDate(), R.drawable.cloudy, (int) df.getTemp().component1()));
+                }
+                MyWeatherAdapter weatherAdapter = new MyWeatherAdapter(getLayoutInflater().getContext(), days_weather_from_server);
+                weather_list.setAdapter(weatherAdapter);
+            }
+
+        }
+
+        @Override
+        public void onLoaderReset(Loader<WeatherForecast> loader) {
+
+        }
     }
 
-    @Override
-    public void onLoaderReset(Loader<WeatherForecast> loader) {
+    class MydailyLoaderManager implements LoaderManager.LoaderCallbacks<CurrentWeather> {
+        @Override
+        public Loader<CurrentWeather> onCreateLoader(int id, Bundle args) {
+            Loader<CurrentWeather> mLoader = null;
+            if (id == LOADER_CD_ID) {
+                mLoader = new MyAsyncTaskLoaderCurrDay(getLayoutInflater().getContext(), args);
+                Log.d("___", "onCreateLoader CurrDay");
+            }
+            return mLoader;
+        }
 
+        @Override
+        public void onLoadFinished(Loader<CurrentWeather> loader, CurrentWeather data) {
+            Log.d("___", "load finished");
+            if (data != null) {
+                temp_num.setText(String.valueOf((int) data.component1()) + "°");
+                humidity_num.setText(String.valueOf(data.component2()) + "%");
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<CurrentWeather> loader) {
+
+        }
     }
-
-    public void startAsyncTask(View view){
-        loaderManager.initLoader(1, null, this);
-    }
-//
-//    public class MyAsyncTaskLoader extends AsyncTaskLoader<List<DailyForecast>> {
-//
-//        public MyAsyncTaskLoader(@NonNull Context context) {
-//            super(context);
-//        }
-//
-//        @Nullable
-//        @Override
-//        public List<DailyForecast> loadInBackground() {
-//            RetrofitClient.INSTANCE.getWeatherForecast().enqueue(new Callback<WeatherForecast>() {
-//                @Override
-//                public void onResponse(Call<WeatherForecast> call, Response<WeatherForecast> response) {
-//                    List<DailyForecast> days_from_server = response.body().component1();
-//                    for (DailyForecast df : days_from_server) {
-//                        days_weather_from_server.add(new DayWeather(df.getDate(), R.drawable.cloudy, (int) df.getTemp().component1()));
-//                    }
-//                    runOnUiThread(new Runnable() {
-//                                      @Override
-//                                      public void run() {
-//                                          MyWeatherAdapter weatherAdapter = new MyWeatherAdapter(getLayoutInflater().getContext(), days_weather_from_server);
-//                                          weather_list.setAdapter(weatherAdapter);
-//                                      }
-//                                  }
-//                    );
-//                }
-//                @Override
-//                public void onFailure(Call<WeatherForecast> call, Throwable t) {
-//
-//                }
-//            });
-//            return null;
-//        }
-//    }
-
-//    class MyLoaderCallbacks implements android.app.LoaderManager.LoaderCallbacks<List<DailyForecast>> {
-//
-//        @NonNull
-//        @Override
-//        public Loader<List<DailyForecast>> onCreateLoader(int id, @Nullable Bundle args) {
-//            return new MyAsyncTaskLoader(getLayoutInflater().getContext());
-//        }
-//
-//        @Override
-//        public void onLoadFinished(android.content.Loader<List<DailyForecast>> loader, List<DailyForecast> data) {
-//            MyWeatherAdapter weatherAdapter = new MyWeatherAdapter(getLayoutInflater().getContext(), days_weather_from_server);
-//            weather_list.setAdapter(weatherAdapter);
-//        }
-//
-//        @Override
-//        public void onLoaderReset(android.content.Loader<List<DailyForecast>> loader) {
-//
-//        }
-//
-//    }
 }
 
 
